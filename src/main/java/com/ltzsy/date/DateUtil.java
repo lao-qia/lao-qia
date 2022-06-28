cf9c7c4ea69a5530913adce20ece368ed4722f7a
ltzsy <450952409@qq.com>
2022-06-28 15:43:38 +0800

    Update DateUtil.java

--- src/main/java/com/ltzsy/date/DateUtil.java
+++ src/main/java/com/ltzsy/date/DateUtil.java
@@ -18,7 +18,44 @@ import java.util.TimeZone;
 public class DateUtil {
 
     public static void main(String[] args) {
-        System.out.println(getCalendar());
+        String str = getCurrentTimeAddMilli(1000l * 60, DATE_FORMAT_yy_MM_dd_1);
+        System.out.println(str);
+    }
+
+    /**
+     * 日期格式：yyyy-MM-dd HH:mm:ss
+     */
+    public static final String DATE_FORMAT_yy_MM_dd_HH_mm_ss = "yyyy-MM-dd HH:mm:ss";
+
+    /**
+     * 日期格式：yyyy-MM-dd
+     */
+    public static final String DATE_FORMAT_yy_MM_dd = "yyyy-MM-dd";
+
+    /**
+     * 日期格式：yyyy/MM/dd HH:mm:ss
+     */
+    public static final String DATE_FORMAT_yy_MM_dd_HH_mm_ss_1 = "yyyy/MM/dd HH:mm:ss";
+
+    /**
+     * 日期格式：yyyy/MM/dd
+     */
+    public static final String DATE_FORMAT_yy_MM_dd_1 = "yyyy/MM/dd";
+
+    /**
+     * @description: 获取Calendard对象 <br>
+     * @version: 1.0 <br>
+     * @author: leevi.li <br>
+     * @create: 2022/6/28 11:33 <br>
+     * @update: 2022/6/28 11:33 <br>
+     * @param:
+     * @return java.util.Calendar
+     */
+    public static Calendar getCalendar(){
+        TimeZone timeZone = TimeZone.getTimeZone("GMT+8");
+        Locale locale = Locale.CHINESE;
+        Calendar calendar = Calendar.getInstance(timeZone, locale);
+        return  calendar;
     }
 
     /**
@@ -33,6 +70,20 @@ public class DateUtil {
     public static String getFormatedCurrentTime(String format){
         Calendar calendar = getCalendar();
         Date date = calendar.getTime();
+        return formatDate(date, format);
+    }
+
+    /**
+     * @description: 格式化时间 <br>
+     * @version: 1.0 <br>
+     * @author: leevi.li <br>
+     * @create: 2022/6/28 15:24 <br>
+     * @update: 2022/6/28 15:24 <br>
+     * @param: date
+     * @param: format
+     * @return java.lang.String
+     */
+    public static String formatDate(Date date, String format){
         SimpleDateFormat sdf = new SimpleDateFormat(format);
         String dateStr = sdf.format(date);
         return dateStr;
@@ -95,18 +146,20 @@ public class DateUtil {
     }
 
     /**
-     * @description: 获取当前时间 <br>
+     * @description: 获取当前时间加上指定毫秒数（可负数）的格式化时间 <br>
      * @version: 1.0 <br>
      * @author: leevi.li <br>
-     * @create: 2022/6/28 11:33 <br>
-     * @update: 2022/6/28 11:33 <br>
-     * @param:
-     * @return java.util.Calendar
+     * @create: 2022/6/28 15:20 <br>
+     * @update: 2022/6/28 15:20 <br>
+     * @param: millisecond 毫秒数
+     * @return java.lang.String
      */
-    public static Calendar getCalendar(){
-        TimeZone timeZone = TimeZone.getTimeZone("GMT+8");
-        Locale locale = Locale.CHINESE;
-        Calendar calendar = Calendar.getInstance(timeZone, locale);
-        return  calendar;
+    public static String getCurrentTimeAddMilli(Long millisecond, String format){
+        Calendar calendar = getCalendar();
+        long millis = calendar.getTimeInMillis();
+        millis += millisecond;
+        calendar.setTimeInMillis(millis);
+        Date date = calendar.getTime();
+        return formatDate(date, format);
     }
 }
